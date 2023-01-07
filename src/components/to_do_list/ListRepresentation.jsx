f726fd188ed11b79d98b0d5fd8cf01a44d85b89f
Eivind Holvik <eivind.holvik@noroff.no>
2023-01-07 02:16:54 +0100

    Fix key problem, the key should be directly on the component, and not the underlying jsx

--- src/components/to_do_list/ListRepresentation.jsx
+++ src/components/to_do_list/ListRepresentation.jsx
@@ -3,7 +3,7 @@ import ToDoItem from "./ToDoItem";
 
 export default function ListRepresentation(props) {
   const displayList = props.ListItemArray.map((item) => {
-    return <ToDoItem item={item} SetList={props.SetList} />;
+    return <ToDoItem key={item.key} item={item} SetList={props.SetList} />;
   });
 
   return <div className="ToDoRepresentation">{displayList}</div>;
--- src/components/to_do_list/ToDoItem.jsx
+++ src/components/to_do_list/ToDoItem.jsx
@@ -4,11 +4,11 @@ import "./ToDoItem.scss";
 export default function ToDoItem(props){
 
   const handleClick = (e) => {
-    const keyValue = e.currentTarget.getAttribute("keyValue");
+    const keyvalue = e.currentTarget.getAttribute("keyvalue");
     props.SetList((prevState) => {
       const tempList = [...prevState];
       tempList.forEach((item) => {
-        if (item.key === keyValue) {
+        if (item.key === keyvalue) {
           item.finished ? item.finished = false : item.finished = true;
         }
       })
@@ -18,6 +18,6 @@ export default function ToDoItem(props){
 
   let classes = `ToDoItem${props.item.finished ? " Finished" : ""}`;
 
-  //Tror ikke man skal trenge å bruke keyValue og key. Men jeg fant det ikke enda
-  return <div className={classes} key={props.item.key} keyValue={props.item.key} onClick={handleClick}><h2>{props.item.content}</h2></div>
+  //Tror ikke man skal trenge å bruke keyvalue og key. Men jeg fant det ikke enda
+  return <div className={classes} keyvalue={props.item.key} onClick={handleClick}><h2>{props.item.content}</h2></div>
 }
